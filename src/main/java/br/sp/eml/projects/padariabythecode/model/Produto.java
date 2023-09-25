bc216810c0b0bb755904331246061233a0ee69d0
EduRTorquato <eduardotorquato78@gmail.com>
2023-09-25 12:46:03 -0300

    Tela Produtos

---
+++ src/main/java/br/sp/eml/projects/padariabythecode/model/Produto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.sp.eml.projects.padariabythecode.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author eduar
+ */
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+    private String codigo;
+    private Date dataValidade;
+}
--- src/main/java/br/sp/eml/projects/padariabythecode/view/TelaPrincipalVendas.java
+++ src/main/java/br/sp/eml/projects/padariabythecode/view/TelaPrincipalVendas.java
@@ -40,7 +40,6 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         txtNomeProduto = new javax.swing.JTextField();
         btnBuscarNomeProduto = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
         txtQtdProduto = new javax.swing.JTextField();
         btnAdicionarProduto = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
@@ -141,7 +140,7 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
 
         btnBuscarNomeProduto.setText("Buscar");
 
-        jLabel4.setText("Quantidade:");
+        txtQtdProduto.setText("Quantidade");
 
         btnAdicionarProduto.setText("Adicionar Produto");
 
@@ -220,10 +219,7 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(pnlPrincipalLayout.createSequentialGroup()
                         .addGroup(pnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(pnlPrincipalLayout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addGap(163, 163, 163)
-                                .addComponent(jLabel4))
+                            .addComponent(jLabel3)
                             .addGroup(pnlPrincipalLayout.createSequentialGroup()
                                 .addGroup(pnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                     .addComponent(txtNomeProduto, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
@@ -244,7 +240,7 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
                                         .addComponent(txtQtdProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                         .addComponent(btnAdicionarProduto)))))
-                        .addGap(0, 87, Short.MAX_VALUE)))
+                        .addGap(0, 91, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         pnlPrincipalLayout.setVerticalGroup(
@@ -261,9 +257,7 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
                     .addComponent(btnBuscarCliente)
                     .addComponent(btnRedirecionarTelaCadClientes))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(pnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4))
+                .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(pnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtNomeProduto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -274,6 +268,8 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
+
+        txtQtdProduto.getAccessibleContext().setAccessibleName("");
 
         pnlCabecalho.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
@@ -443,6 +439,11 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
         mnuArquivos.add(mnuItemCadatroClientes);
 
         mnuItemCadastroProdutos.setText("Cadastro Produtos");
+        mnuItemCadastroProdutos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuItemCadastroProdutosActionPerformed(evt);
+            }
+        });
         mnuArquivos.add(mnuItemCadastroProdutos);
 
         mnuItemRelatorioVendas.setText("Relatório de Vendas");
@@ -489,6 +490,10 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void mnuItemCadastroProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuItemCadastroProdutosActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mnuItemCadastroProdutosActionPerformed
 
     /**
      * @param args the command line arguments
@@ -540,7 +545,6 @@ public class TelaPrincipalVendas extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel8;
